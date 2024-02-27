1ebde28a893e57316a8bb1038229c6d6e15e669a
PegJac <peggy.jacobson@outlook.com>
2024-02-27 23:14:45 +0100

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>:))</p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
